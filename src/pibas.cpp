9b28bea0e035f621d3efda78218f919f1fb22b7b
larrybal <larrybalagula@gmail.com>
2025-02-11 23:42:10 -0800

    Initializing client/server sockets for Search

--- src/pibas.cpp
+++ src/pibas.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <list>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 #include "pibas.hpp"
 #include "encryption.hpp"
+#include <unistd.h>
+
 #define KEYLEN 100
 using namespace std;
 
@@ -27,8 +34,55 @@ std::vector<std::string> Search(
     const std::string &K,
     const std::string &w)
 {
-
     std::vector<std::string> result;
-    // Implementation: Decrypt ED[w] using K and return document IDs
+    
+    // Initializing client setup
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // specifying address
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    // sending connection request
+    connect(clientSocket, (struct sockaddr*)&serverAddress,
+            sizeof(serverAddress));
+
+    // sending data
+    const char* message = "Hello, server!";
+    send(clientSocket, message, strlen(message), 0);
+
+    // closing socket
+    close(clientSocket);
+
+    // Initializing server setup
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Defining server address
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    // Bind the socket and have 1 as listening log for one range query at a time
+    bind(serverSocket, (struct sockaddr*)&serverAddress,
+        sizeof(serverAddress));
+
+    
+    listen(serverSocket, 1);
+
+    // Accept connection request
+    int clientSocket
+        = accept(serverSocket, nullptr, nullptr);
+
+    char buffer[1024] = { 0 };
+    recv(clientSocket, buffer, sizeof(buffer), 0);
+    cout << "Message from client: " << buffer
+                << endl;
+
+    // closing the socket.
+    close(serverSocket);
+    
     return result;
 }
