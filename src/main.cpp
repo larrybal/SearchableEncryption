8cff4260619cd4a6854df80b1531af9d9936311f
larrybal <larrybalagula@gmail.com>
2025-03-10 11:39:24 -0700

    Finishing range query testing

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,24 @@
 #include <string>
 
 int main() {
-    // Create an unordered_map to represent a simple dataset
+    // 1. Create a synthetic dataset (map of Age to tuple IDs)
     // Key, Value
     std::unordered_map<std::string, std::string> dataset;
-    
+    dataset["27"] = {"emp1", "emp2"};
+    dataset["28"] = {"emp3"};
+    dataset["29"] = {"emp4", "emp5", "emp6"};
+    dataset["30"] = {"emp7"};
+    dataset["31"] = {"emp8", "emp9"};
+
+    // Master key
+    std::string masterKey = "masterkey";
+
+    // 2. Run Setup and generate the encrypted index ED
+    std::map<std::string, std::string> ED = Setup(dataset, masterKey);
+
+    // 3. For a given age (point query), generate search token, run Search, and decrypt results
+    std::string searchAge = "27"; 
+    std::vector<std::string> searchResults = Search(ED, masterKey, searchAge);
 
     // Printing dataset contents 
     std::cout << "Dataset Contents:" << std::endl;
