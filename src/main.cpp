b30ce8e74117e5a779c06e8c1c295fa53ad84d32
larrybal <larrybalagula@gmail.com>
2025-03-10 11:27:59 -0700

    refactor

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "encryption.hpp"
+#include "pibas.hpp"
+
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+int main() {
+    // Create an unordered_map to represent a simple dataset
+    // Key, Value
+    std::unordered_map<std::string, std::string> dataset;
+    
+
+    // Printing dataset contents 
+    std::cout << "Dataset Contents:" << std::endl;
+
+    return 0;
+}
