e40d4ca5f8414f46f350d9e995980cb2063a24d4
Benjamin Mencer <benji@realitydefender.ai>
2025-03-11 13:05:56 -0700

    master key gone

--- src/main.cpp
+++ src/main.cpp
@@ -16,11 +16,6 @@ int main()
     dataset["30"] = {"emp7"};
     dataset["31"] = {"emp8", "emp9"};
 
-    // Master key
-    const vector<unsigned char> masterKey(
-        reinterpret_cast<const unsigned char *>("masterkey"),
-        reinterpret_cast<const unsigned char *>("masterkey") + strlen("masterkey"));
-
     // 2. Run Setup and generate the encrypted index ED
     pair<vector<unsigned char>, std::map<std::string, std::vector<unsigned char>>> K_ED = Pibas::Setup(dataset);
     vector<unsigned char> K = K_ED.first;
@@ -28,7 +23,7 @@ int main()
 
     // 3. For a given age (point query), generate search token, run Search, and decrypt results
     std::string searchAge = "27";
-    std::vector<std::string> searchResults = Pibas::Search(ED, masterKey, searchAge);
+    std::vector<std::string> searchResults = Pibas::Search(ED, K, searchAge);
 
     // Printing dataset contents
     std::cout << "Dataset Contents:" << std::endl;
